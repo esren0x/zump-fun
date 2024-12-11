32a1602400144920bd31a986796af6492231eb07
mm <martin@provable.com>
2024-12-11 10:02:11 -0800

    initial-2

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 
+
 function App() {
   // const { publicKey, requestTransaction, requestRecordPlaintexts, decrypt } = useWallet();
 
