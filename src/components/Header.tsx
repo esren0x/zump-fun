8e6a8a80de627602f105fe7c98698fe77357f99c
Jake Tarnow <jake@provable.com>
2024-12-11 11:33:06 -0800

    thumpin frontend

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,13 @@ import { WalletMultiButton } from '@demox-labs/aleo-wallet-adapter-reactui';
 function Header() {
   return (
     <div className="header">
-        <div style={{minWidth: "150px"}}></div>
+         <div style={{minWidth: "150px"}}></div>
     <div className="header-right">
       <div className="user-info-dropdown">
         <WalletMultiButton />
       </div>
     </div>
-  </div>  
+   </div>
   );
 }
 
--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -11,56 +11,21 @@ function MainContent() {
   
 
   return (
-    <div className={`main-container`}>
-      
+      <div className={`main-container`}>
 
-      <div className="card-box pd-20 height-100-p mb-30">
+
         <div className="row align-items-center">
-          <div className="col-md-4">
-            <img src="vendors/images/banner-img.png" alt="" />
-          </div>
+          <img src="vendors/images/banner-img.png" alt=""/>
           <div className="col-md-8">
-            <h4 className="font-20 weight-500 mb-10 text-capitalize">
-                <p className="welcome-to">
-                  ZUMP.FUN
-                </p>
-            </h4>
+            <p className="welcome-to">
+              <img src="src/assets/Lion_8bit_04.png" width={100}/>
+              ZUMP.FUN
+              <img src="src/assets/Lion_8bit_04.png" width={100}/>
+            </p>
           </div>
         </div>
+        <div><img src="src/assets/runningLion.gif" width={500}/></div>
       </div>
-
-      <div className="card-box pb-10">
-        <div className="h5 pd-20 mb-0"><i className="icon-copy dw dw-microphone-11"></i> My Zumps</div>
-        <table className="data-table table nowrap">
-          <thead>
-            <tr>
-              <th className="table-plus">Addr</th>
-              <th>Symbol</th>
-              <th>Supply</th>
-              <th className="datatable-nosort">Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {sentMessages.map((msg, index) => (
-              <tr key={index}>
-                <td className="table-plus">
-                  <div className="name-avatar d-flex align-items-center">
-                    <div className="avatar mr-2 flex-shrink-0">
-                      <img src="/assets/anon2.jpeg" alt="Avatar" />
-                    </div>
-                    <div className="txt"><div className="weight-600">{publicKey || 'N/A'}</div></div>
-                  </div>
-                </td>
-                <td>{new Date(msg.date * 1000).toLocaleString()}</td>
-                <td><span className="badge badge-pill" data-bgcolor="#e7ebf5" data-color="#265ed7">sent|view</span></td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      
-    </div>
   );
 }
 
